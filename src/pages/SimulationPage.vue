aa6846319507c710234d4efb499c6261bcc269ea
Maria Baeta <m.martinsdg@gmail.com>
2023-11-18 23:35:48 -0300

    feat: Iniciando projeto

---
+++ src/pages/SimulationPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <SimulationsTable></SimulationsTable>
+  </q-page>
+</template>
+
+<script lang="ts">
+import SimulationsTable from 'src/components/SimulationsTable.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: { SimulationsTable },
+});
+</script>
